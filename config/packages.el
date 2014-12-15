05244d2c1b2222fc6e544359d36e11d4f0e70832
Gregory Ganley <gcganley2854@gmail.com>
2014-12-15 11:17:39 -0500

    factor out package stuff

---
+++ config/packages.el
@@ -0,0 +1,56 @@
+(require 'package)
+(require 'cl-lib)
+(eval-when-compile (require 'cl))
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+(defvar gcg/packages
+  '(cl-lib
+    company
+    haskell-mode
+    rainbow-delimiters
+    smartparens
+    smex
+    flycheck
+    magit
+    solarized-theme
+    company
+    company-ghc))
+(defun packages-installed-p ()
+  (every #'package-installed-p gcg/packages))
+(defun require-package (package)
+  (unless (memq package gcg/packages)
+    (add-to-list 'gcg/packages package))
+  (unless (package-installed-p package)
+    (package-install package)))
+(defun require-packages (packages)
+  (mapc #'require-package packages))
+(defun install-packages ()
+  (unless (packages-installed-p)
+    (package-refresh-contents)
+    (require-packages gcg/packages)))
+
+(install-packages)
+(defun list-foreign-packages ()
+  (interactive)
+  (package-show-package-list
+   (set-difference package-activated-list gcg/packages)))
+(defmacro auto-install (extension package mode)
+  `(add-to-list 'auto-mode-alist
+		`(,extension . (lambda ()
+				 (unless (package-installed-p ',package)
+				   (package-install ',package))
+				 (,mode)))))
+
+(defvar auto-install-alist
+  '(("\\.hs\\'" haskell-mode haskell-mode)
+    ("\\.clj\\'" clojure-mode clojure-mode)))
+(mapc
+ (lambda (entry)
+   (let ((extension (car entry))
+	 (package (cadr entry))
+	 (mode (cadr (cdr entry))))
+     (unless (package-installed-p package)
+       (auto-install extension package mode))))
+ auto-install-alist)
+(provide 'packages)
--- init.el
+++ init.el
@@ -1,61 +1,7 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(require 'cl)
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
-(defvar gcg/packages
-  '(haskell-mode
-    rainbow-delimiters
-    paredit
-    auto-complete
-    smartparens
-    smex
-    flycheck
-    magit
-    solarized-theme
-    volatile-highlights
-    ac-haskell-process))
-
-(defun packages-installed-p ()
-  (every #'package-installed-p gcg/packages))
-(defun require-package (package)
-  (unless (memq package gcg/packages)
-    (add-to-list 'gcg/packages package))
-  (unless (package-installed-p package)
-    (package-install package)))
-(defun require-packages (packages)
-  (mapc #'require-package packages))
-(defun install-packages ()
-  (unless (packages-installed-p)
-    (package-refresh-contents)
-    (require-packages gcg/packages)))
-(install-packages)
-(defun list-foreign-packages ()
-  (interactive)
-  (package-show-package-list
-   (set-difference package-activated-list gcg/packages)))
-(defmacro auto-install (extension package mode)
-  `(add-to-list 'auto-mode-alist
-                `(,extension . (lambda ()
-                                 (unless (package-installed-p ',package)
-                                   (package-install ',package))
-                                 (,mode)))))
-(defvar auto-install-alist
-  '(("\\.hs\\'" haskell-mode haskell-mode)
-    ("\\.clj\\'" clojure-mode clojure-mode)
-    ("\\.css\\'" css-mode css-mode)
-    ("\\.html\\'" web-mode web-mode)))
-(mapc
- (lambda (entry)
-   (let ((extension (car entry))
-         (package (cadr entry))
-         (mode (cadr (cdr entry))))
-     (unless (package-installed-p package)
-       (auto-install extension package mode))))
-   auto-install-alist)
-
+(add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+(require 'packages)
 (require 'solarized)
 ;;; init.el ends here
 (custom-set-variables
@@ -63,42 +9,41 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-backends
+   (quote
+    (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-ropemacs company-cmake company-capf
+		  (company-dabbrev-code company-gtags company-etags company-keywords)
+		  company-oddmuse company-files company-dabbrev company-gtags company-ghc)))
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(haskell-process-type 'cabal-repl)
- '(haskell-process-args-cabal-repl
-   '("--ghc-option=ferror-spans" "--with-ghc=ghci-ng"))
+ '(haskell-complete-module-preferred
+   (quote
+    ("Data.ByteString" "Data.ByteString.Lazy" "Data.List" "Data.Maybe" "Data.Monoid")))
+ '(haskell-interactive-mode-eval-mode (quote haskell-mode))
+ '(haskell-interactive-mode-eval-pretty nil)
+ '(haskell-interactive-mode-include-file-name nil)
  '(haskell-notify-p t)
+ '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng")))
+ '(haskell-process-args-ghci (quote ("-ferror-spans")))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-generate-tags nil)
+ '(haskell-process-log t)
+ '(haskell-process-path-ghci "ghci-ng")
+ '(haskell-process-reload-with-fbytecode nil)
+ '(haskell-process-suggest-haskell-docs-imports t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote cabal-repl))
+ '(haskell-process-use-presentation-mode t)
  '(haskell-stylish-on-save nil)
  '(haskell-tags-on-save nil)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-reload-with-fbytecode nil)
- '(haskell-process-use-presentation-mode t)
- '(haskell-interactive-mode-include-file-name nil)
- '(haskell-interactive-mode-eval-pretty nil)
- '(shm-use-presentation-mode t)
- '(shm-auto-insert-skeletons t)
- '(haskell-process-suggest-haskell-docs-imports t)
  '(hindent-style "chris-done")
- '(haskell-interactive-mode-eval-mode 'haskell-mode)
- '(haskell-process-path-ghci "ghci-ng")
- '(haskell-process-args-ghci '("-ferror-spans"))
- '(haskell-process-args-cabal-repl
-   '("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng"))
- '(haskell-process-generate-tags nil)
- '(haskell-complete-module-preferred
-   '("Data.ByteString"
-     "Data.ByteString.Lazy"
-     "Data.List"
-     "Data.Maybe"
-     "Data.Monoid"))
  '(inhibit-default-init t)
  '(inhibit-startup-screen t)
- '(initial-buffer-choice t))
+ '(initial-buffer-choice t)
+ '(shm-auto-insert-skeletons t)
+ '(shm-use-presentation-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -116,7 +61,6 @@
 (require 'haskell-process)
 (require 'haskell-interactive-mode)
 (require 'haskell-font-lock)
-
 
 (defun haskell-process-all-types ()
   (interactive)
@@ -167,18 +111,25 @@
 (setq sp-autoskip-closing-pair 'always)
 (setq sp-hybrid-kill-entire-symbol nil)
 (sp-use-paredit-bindings)
-(smartparens-global-mode +1)
-(rainbow-delimiters-mode +1)
+(smartparens-global-mode t)
+(rainbow-delimiters-mode t)
 (sp-pair "'" nil :actions :rem)
-
-(require 'auto-complete-config)
-(global-auto-complete-mode t)
-(setq-default ac-expand-on-auto-comlete nil)
-(setq-default ac-auto-start nil)
-(setq-default ac-dwim nil)
-
-(setq tab-always-indent 'complete)
-(add-to-list 'completion-styles 'initials t)
 
 (set-frame-font "Anonymous Pro-8" nil t)
 (setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+
+;;  Hooks
+;;; Company
+(add-hook 'after-init-hook 'global-company-mode)
+
+;;; Haskell
+
+(add-hook 'haskell-mode-hook 'structured-haskell-mode)
+
+;;  List
+;;; Company
+
+;;  Key Bindings
+;;; Company
+(global-set-key (kbd "C-c SPC") 'company-complete)
+;;; Haskell
