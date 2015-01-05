4448d7fee528e46fbb55580a359edd0108787f9e
Gregory Ganley <gcganley@gcganley.com>
2015-01-05 00:36:58 +0000

    asf

---
+++ config/global-gcg.el
@@ -0,0 +1,39 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-backends
+   (quote
+    (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-ropemacs company-cmake company-capf
+		  (company-dabbrev-code company-gtags company-etags company-keywords)
+		  company-oddmuse company-files company-dabbrev company-gtags company-ghc)))
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(inhibit-default-init t)
+ '(inhibit-startup-screen t))
+
+;editor and editing
+(blink-cursor-mode -1)
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+(fringe-mode 4)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+					; edititing
+
+(require 'paredit)
+(require 'rainbow-delimiters)
+
+(autoload 'enable-paredit-mode "paredit" "asdf" t)
+(add-hook 'prog-mode-hook 'enable-paredit-mode)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+(global-set-key (kbd "C-x C-S") 'write-file)
+
+(set-frame-font "Anonymous Pro-8" nil t)
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+(provide 'global-gcg)
---
+++ config/haskell-gcg.el
@@ -0,0 +1,144 @@
+(require 'haskell-mode)
+(require 'hindent)
+(require 'shm)
+(require 'haskell-process)
+(require 'haskell-simple-indent)
+(require 'haskell-font-lock)
+(require 'haskell-debug)
+
+(custom-set-variables
+ '(haskell-process-type 'ghci)
+ '(haskell-process-args-ghci '())
+ '(haskell-notify-p t)
+ '(haskell-stylish-on-save nil)
+ '(haskell-tags-on-save nil)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-reload-with-fbytecode nil)
+ '(haskell-process-use-presentation-mode t)
+ '(haskell-interactive-mode-include-file-name nil)
+ '(haskell-interactive-mode-eval-pretty nil)
+ '(haskell-process-do-cabal-format-string ":!cd %s && unset GHC_PACKAGE_PATH && %s")
+ '(shm-use-hdevtools t)
+ '(shm-use-presentation-mode t)
+ '(shm-auto-insert-skeletons t)
+ '(shm-auto-insert-bangs t)
+ '(haskell-process-show-debug-tips nil)
+ '(haskell-process-suggest-hoogle-imports nil)
+ '(haskell-process-suggest-haskell-docs-imports t)
+ '(hindent-style "chris-done")
+ '(haskell-interactive-mode-eval-as-mode 'haskell-mode)
+ '(haskell-process-path-ghci "ghci-ng")
+ '(haskell-process-args-ghci '("-ferror-spans"))
+ '(haskell-process-args-cabal-repl '("--ghc-option=ferror-spans" "--with-ghc=ghci-ng"))
+ '(haskell-process-generate-tags nil))
+
+(defun haskell-insert-doc ()
+  "Insert the documentation syntax."
+  (interactive)
+  (insert "-- | "))
+
+(defvar haskell-process-use-ghci nil)
+
+(defun haskell-process-cabal-build-and-restart ()
+  "Build and restart the Cabal project."
+  (interactive)
+  (cond
+   (haskell-process-use-ghci
+    (when (buffer-file-name)
+      (save-buffer))
+    ;; Reload main module where `main' function is
+    (haskell-process-reload-devel-main))
+   (t
+    (haskell-process-cabal-build)
+    (haskell-process-queue-without-filters
+     (haskell-process)
+     (format ":!cd %s && scripts/restart\n" (haskell-session-cabal-dir (haskell-session)))))
+   (t (turbo-devel-reload))))
+
+(defun shm-contextual-space ()
+  "Do contextual space first, and run shm/space if no change in
+the cursor position happened."
+  (interactive)
+  (if (looking-back "import")
+        (call-interactively 'haskell-mode-contextual-space)
+      (progn
+        (let ((ident (haskell-ident-at-point)))
+          (when ident
+            (and interactive-haskell-mode
+                 (haskell-process-do-try-type ident))))
+        (call-interactively 'shm/space))))
+
+   (defun shm/insert-putstrln ()
+     "Insert a putStrLn."
+     (interactive)
+     (let ((name
+	    (save-excursion
+	      (goto-char (car (shm-decl-points)))
+	      (buffer-substring-no-properties
+	       (point)
+	       (1- (search-forward " "))))))
+       (insert
+	(format "putStrLn \"%s:%s:%d\""
+		(file-name-nondirectory (buffer-file-name))
+		name
+		(line-number-at-pos))))))
+
+  (add-hook 'haskell-mode-hook 'structured-haskell-mode))
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+(add-hook 'haskell-interactive-mode-hook 'structured-haskell-repl-mode)
+
+(define-key haskell-mode-map [f5] 'haskell-process-load-or-reload)
+(define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+(define-key haskell-mode-map [f9] 'haskell-interactive-mode-visit-error)
+(define-key haskell-mode-map [f11] 'haskell-process-cabal-build)
+(define-key haskell-mode-map [f12] 'haskell-process-cabal-build-and-restart)
+(define-key haskell-mode-map (kbd "<space>") 'haskell-mode-contextual-space)
+(define-key haskell-mode-map (kbd "-") 'smart-hyphen)
+(define-key haskell-mode-map (kbd "C-<return>") 'haskell-simple-indent-newline-indent)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "C-c i") 'hindent/reformat-decl)
+(define-key haskell-mode-map (kbd "C-c C-a") 'haskell-insert-doc)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
+(define-key haskell-mode-map (kbd "C-c C-v") 'haskell-interactive-toggle-print-mode)
+
+(define-key haskell-cabal-mode-map [f9] 'haskell-interactive-mode-visit-error)
+(define-key haskell-cabal-mode-map [f11] 'haskell-process-cabal-build)
+(define-key haskell-cabal-mode-map [f12] 'haskell-process-cabal-build-and-restart)
+(define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
+(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+
+(define-key shm-map (kbd "C-c C-p") 'shm/expand-pattern)
+(define-key shm-map (kbd ",") 'shm-comma-god)
+(define-key shm-map (kbd "C-c C-s") 'shm/case-split)
+(define-key shm-map (kbd "SPC") 'shm-contextual-space)
+(define-key shm-map (kbd "C-\\") 'shm/goto-last-point)
+(define-key shm-map (kbd "C-c C-f") 'shm-fold-toggle-decl)
+(define-key shm-map (kbd "C-c i") 'shm-reformat-decl)
+
+(defun haskell-process-all-types ()
+  "List all types in a grep-mode buffer."
+  (interactive)
+  (let ((session (haskell-session)))
+    (switch-to-buffer (get-buffer-create (format "*%s:all-types*"
+                                                 (haskell-session-name (haskell-session)))))
+    (setq haskell-session session)
+    (cd (haskell-session-current-dir session))
+    (let ((inhibit-read-only t))
+      (erase-buffer)
+      (let ((haskell-process-log nil))
+        (insert (haskell-process-queue-sync-request (haskell-process) ":all-types")))
+      (unless (eq major-mode  'compilation-mode)
+        (compilation-mode)
+        (setq compilation-error-regexp-alist
+              haskell-compilation-error-regexp-alist)))))
+(provide 'haskell-gcg)
