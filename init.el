94dd3da5078bbefc5ef452e862239bb3ea5d3691
gganley <gganley@student.bridgew.edu>
2015-07-26 10:48:52 +0000

    fixed what git fucks up

--- init.el
+++ init.el
@@ -5,15 +5,10 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 ; bring in all of my .el files
-<<<<<<< HEAD
-(require 'gganley-packages)
-(require 'gganley-haskell)
-(require 'gganley-global)
-=======
 
 (require 'gganley-packages)
+(require 'gganley-haskell)
 (require 'gganley-config)
 (require 'gganley-haskell)
->>>>>>> f5952e8bdfb285ec804f4d69abcde57f67be7a2a
 (require 'gganley-js)
 (require 'gganley-lisp)
