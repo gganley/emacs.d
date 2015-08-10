eabd40a714b88cdcd2b51f98d179f1c11d917e3d
gganley <gganley@student.bridgew.edu>
2015-08-10 23:50:55 +0000

    fuckign themes

--- init.el
+++ init.el
@@ -17,19 +17,3 @@
 (load-user-file "gganley/lisp.el")
 (load-user-file "gganley/haskell.el")
 (load-user-file "gganley/keybindings.el")
-
-(load-theme 'solarized-light)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
