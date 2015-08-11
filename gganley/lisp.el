fc66161ffa83117a73d04ee2a150f32a75ea2740
gganley <gganley@student.bridgew.edu>
2015-08-11 09:07:59 +0000

    removed keybindings and hooks from lisp.el

--- gganley/lisp.el
+++ gganley/lisp.el
@@ -1,7 +1,5 @@
 (require 'smartparens)
-(add-hook 'prog-mode-hook 'smartparens-strict-mode)
-(add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
-(define-key smartparens-mode-map (kbd ")") 'sp-up-sexp)
+
 
 (sp-pair "'" nil :actions :rem)
 (sp-pair "`" nil :actions :rem)
