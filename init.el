7ecff6e284632939257f959a116b17d811b3f178
Gregory Ganley <gganley@student.bridgew.edu>
2017-01-24 13:03:20 -0500

    Change org-mode to be fixed width

--- init.el
+++ init.el
@@ -22,3 +22,13 @@
 (load-user-file "gganley/hooker.el")
 (load-user-file "gganley/python.el")
 (load-user-file "gganley/c.el")
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit default :foreground "#cb4b16"))))
+ '(org-level-2 ((t (:inherit default :foreground "#808000"))))
+ '(org-level-3 ((t (:inherit default :foreground "#859900"))))
+ '(org-level-4 ((t (:inherit default :foreground "#b58900"))))
+ '(org-level-5 ((t (:inherit default :foreground "#2aa198"))))
+ '(org-level-6 ((t (:inherit default :foreground "#859900"))))
+ '(org-level-7 ((t (:inherit default :foreground "#dc322f"))))
+ '(org-level-8 ((t (:inherit default :foreground "#268bd2")))))
