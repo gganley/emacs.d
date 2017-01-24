c8d87dee7a39d33340710bfd1d3bcce11dc212de
Gregory Ganley <gganley@student.bridgew.edu>
2017-01-24 12:43:26 -0500

    Remove dumb keybindings

--- gganley/keybindings.el
+++ gganley/keybindings.el
@@ -13,21 +13,8 @@
 (global-set-key (kbd "C-c l") 'windmove-up)
 (global-set-key (kbd "C-c ;") 'windmove-right)
 
-;; Set it to something sensible
-(global-set-key (kbd "C-+") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
-
 ;; Org shit
 (global-set-key (kbd "C-c c") 'org-capture)
-
-;; Magit
-(global-set-key (kbd "C-c g") 'magit-status)
-
-;; Smart parens
-(global-set-key (kbd "C-)") 'sp-forward-slurp-sexp)
-(global-set-key (kbd "C-(") 'sp-backward-slurp-sexp)
-(global-set-key (kbd "C-M-)") 'sp-forward-barf-sexp)
-(global-set-key (kbd "C-M-(") 'sp-backward-barf-sexp)
 
 ;; Magit
 (global-set-key (kbd "C-c g") 'magit-status)
@@ -37,10 +24,5 @@
              [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
              [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
   (global-unset-key k))
-(define-key js-mode-map "{" 'paredit-open-curly)
-(define-key js-mode-map "}" 'paredit-close-curly-and-newline)
-
-(define-key smartparens-mode-map (kbd ")") 'sp-up-sexp)
-
 
 (provide 'keybindings)
