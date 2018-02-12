066b61982ad28696362e3e506cc46a7195fb5d3f
gganley <gganley@student.bridgew.edu>
2018-02-12 11:22:49 -0500

    Remove crux configuration

--- init.el
+++ init.el
@@ -109,40 +109,6 @@
 
 (require 'use-package)
 
-(require 'crux)
-(use-package crux
-  :ensure t
-  ;; :bind (("C-c o" . crux-open-with)
-  ;;        ("M-o" . crux-smart-open-line)
-  ;;        ("C-c n" . crux-cleanup-buffer-or-region)
-  ;;        ("C-c f" . crux-recentf-find-file)
-  ;;        ("C-M-z" . crux-indent-defun)
-  ;;        ("C-c u" . crux-view-url)
-  ;;        ("C-c e" . crux-eval-and-replace)
-  ;;        ("C-c w" . crux-swap-windows)
-  ;;        ("C-c D" . crux-delete-file-and-buffer)
-  ;;        ("C-c r" . crux-rename-buffer-and-file)
-  ;;        ("C-c t" . crux-visit-term-buffer)
-  ;;        ("C-c k" . crux-kill-other-buffers)
-  ;;        ("C-c TAB" . crux-indent-rigidly-and-copy-to-clipboard)
-  ;;        ("C-c I" . crux-find-user-init-file)
-  ;;        ("C-c S" . crux-find-shell-init-file)
-  ;;        ("s-r" . crux-recentf-find-file)
-  ;;        ("s-j" . crux-top-join-line)
-  ;;        ("C-^" . crux-top-join-line)
-  ;;        ("s-k" . crux-kill-whole-line)
-  ;;        ("C-<backspace>" . crux-kill-line-backwards)
-  ;;        ("s-o" . crux-smart-open-line-above)
-  ;;        ([remap move-beginning-of-line] . crux-move-beginning-of-line)
-  ;;        ([(shift return)] . crux-smart-open-line)
-  ;;        ([(control shift return)] . crux-smart-open-line-above)
-  ;;        ([remap kill-whole-line] . crux-kill-whole-line)
-  ;;        ("C-c s" . crux-ispell-word-then-abbrev))
-  :config
-  (define-key emacs-lisp-mode-map (kbd "C-c C-z")
-    (lambda ()
-      (interactive)
-      (crux-start-or-switch-to 'ielm "*ielm*"))))
 
 (use-package ielm
   :config
