6fd3d0a1cb11a22d59a4abea67a168e17adeea08
gganley <gganley@student.bridgew.edu>
2018-02-15 10:52:27 -0500

    Configure proper themeing

--- init.el
+++ init.el
@@ -28,6 +28,7 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (require 'init-elpa)
+(require 'init-themes)
 (require 'init-exec-path)
 (require 'init-editor)
 (require 'init-os)
