4a6349b8619e9935eeda8724f82c3e58c0fbb18d
gganley <gganley@student.bridgew.edu>
2018-02-24 15:28:18 -0500

    Add company mode hook since cider-repl does not derive prog-mode

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -12,10 +12,11 @@
   :init
   (add-hook 'cider-mode-hook #'eldoc-mode)
   (add-hook 'cider-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'cider-mode-hook (lambda () (clj-refactor-mode 1)))
   (add-hook 'cider-repl-mode-hook #'eldoc-mode)
   (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'cider-repl-mode-hook (lambda () (clj-refactor-mode 1)))
-  (add-hook 'cider-mode-hook (lambda () (clj-refactor-mode 1)))
+  (add-hook 'cider-repl-mode-hook #'company-mode)
   :config
   (setq cider-repl-display-help-banner nil
         cider-save-file-on-load t
