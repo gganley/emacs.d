ee7cfb9cf6399bce115696ee035b868d2049cc06
gganley <gganley@student.bridgew.edu>
2018-02-24 14:39:55 -0500

    Refactor configuration out of custom file

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -8,11 +8,18 @@
 
 (use-package cider
   :ensure t
-  :config
-  (setq cider-repl-display-help-banner nil)
+  :init
   (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'cider-repl-mode-hook #'eldoc-mode)
-  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode))
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'cider-repl-mode-hook (lambda () (clj-refactor-mode 1)))
+  (add-hook 'cider-mode-hook (lambda () (clj-refactor-mode 1)))
+  :config
+  (setq cider-repl-display-help-banner nil
+        cider-save-file-on-load t
+        cider-pprint-fn 'pprint
+        cider-repl-pop-to-buffer-on-connect 'display-only))
 
 (use-package clj-refactor
   :ensure t
