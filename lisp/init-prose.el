85193d9844d18a7f6c160e4c9ea547653ad34c98
gganley <gganley@student.bridgew.edu>
2018-03-03 21:28:01 -0500

    Auto `gfm-mode` onto CHANGELOG's

--- lisp/init-prose.el
+++ lisp/init-prose.el
@@ -2,6 +2,7 @@
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
+         ("CHANGELOG\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multi-markdown"
