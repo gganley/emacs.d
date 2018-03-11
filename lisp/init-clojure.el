6c84f2713fba4a228a773c9d4c68199b81ae3b42
gganley <gganley@student.bridgew.edu>
2018-03-11 11:47:52 -0400

    Add fuzz-completion to `cider` through `company-mode`

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -18,10 +18,14 @@
   (add-hook 'cider-mode-hook #'eldoc-mode)
   (add-hook 'cider-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'cider-mode-hook (lambda () (clj-refactor-mode 1)))
+  (add-hook 'cider-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
   (add-hook 'cider-repl-mode-hook #'eldoc-mode)
   (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'cider-repl-mode-hook (lambda () (clj-refactor-mode 1)))
   (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+
   :config
   (setq cider-repl-display-help-banner nil
         cider-save-file-on-load t
