eaff52683677d9bd4a0246f4fefac32937304b1b
gganley <gganley@student.bridgew.edu>
2018-03-12 18:15:32 -0400

    Update how windows are split

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -8,6 +8,22 @@
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 
+(setq split-height-threshold 120)
+(setq split-width-threshold 160)
+
+(defun my-split-window-sensibly (&optional window)
+  "replacement `split-window-sensibly' function which prefers vertical splits"
+  (interactive)
+  (let ((window (or window (selected-window))))
+    (or (and (window-splittable-p window t)
+             (with-selected-window window
+               (split-window-right)))
+        (and (window-splittable-p window)
+             (with-selected-window window
+               (split-window-below))))))
+
+(setq split-window-preferred-function #'my-split-window-sensibly)
+
 (setq frame-title-format
       '((:eval (if (buffer-file-name)
                    (abbreviate-file-name (buffer-file-name))
