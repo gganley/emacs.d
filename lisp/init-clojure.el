9405c81fdee563176b60a2c2097184be51d2502a
gganley <gganley@student.bridgew.edu>
2018-03-12 18:14:59 -0400

    Remove `cider-eval-spinner` since it holds up the REPL somehow

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -1,4 +1,3 @@
-
 (require 'init-lisp)
 
 (use-package clojure-mode
@@ -14,7 +13,8 @@
 
 (use-package cider
   :ensure t
-  :init
+  :after clojure
+  :config
   (add-hook 'cider-mode-hook #'eldoc-mode)
   (add-hook 'cider-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'cider-mode-hook (lambda () (clj-refactor-mode 1)))
@@ -25,15 +25,15 @@
   (add-hook 'cider-repl-mode-hook (lambda () (clj-refactor-mode 1)))
   (add-hook 'cider-repl-mode-hook #'company-mode)
   (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
-
-  :config
   (setq cider-repl-display-help-banner nil
         cider-save-file-on-load t
         cider-pprint-fn 'pprint
-        cider-repl-pop-to-buffer-on-connect 'display-only))
+        cider-repl-pop-to-buffer-on-connect 'display-only
+        cider-show-eval-spinner nil))
 
 (use-package clj-refactor
   :ensure t
+  :after cider
   :diminish
   :init
   (add-hook 'clojure-mode-hook (lambda () (clj-refactor-mode 1)))
