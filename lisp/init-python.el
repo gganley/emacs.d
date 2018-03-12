3026010fbd641a18b93f7e711edf9fb7369cd59a
gganley <gganley@student.bridgew.edu>
2018-03-12 18:16:48 -0400

    ERMERGERD PYTHON

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,14 +1,21 @@
 (use-package elpy
   :ensure t
   :mode ("\\.py\\'" . python-mode)
-  :init
-  (with-eval-after-load 'python (elpy-enable))
-  (elpy-enable)
-  (elpy-company-backend)
+  :after python
+  :commands elpy-enable
+  :init (with-eval-after-load 'python
+          (elpy-enable)
+          (remove-hook 'elpy-modules 'elpy-module-flymake))
   :config
+  (setq elpy-modules '(elpy-module-sane-defaults
+                       elpy-module-company
+                       elpy-module-eldoc
+                       elpy-module-highlight-indentation
+                       elpy-module-pyvenv
+                       elpy-module-yasnippet))
   (add-hook 'python-mode-hook #'company-mode)
   (add-hook 'python-mode-hook #'smartparens-strict-mode)
-  (setq python-shell-interpreter "python3")
-  )
+  (add-hook 'python-mode-hook #'pyenv-mode)
+  (setq python-shell-interpreter "python3"))
 
 (provide 'init-python)
