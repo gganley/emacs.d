79f8e9e4260ddc0456062e3dc29ae2048297662d
gganley <gganley@student.bridgew.edu>
2018-10-28 19:00:35 -0400

    Commit or something before use-package

--- init.el
+++ init.el
@@ -21,12 +21,11 @@
       debug-on-quit nil
       inhibit-startup-screen t
       inhibit-startup-echo-area-message "Hello GGANLEY")
-
-
+(set-default-coding-systems 'utf-8)
 
 (add-hook 'prog-mode-hook 'linum-mode)
 
-(menu-bar-mode -1)
+
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 
@@ -50,7 +49,7 @@
 (require 'cl-lib)
 
 (defvar my-packages
-  '(spacemacs-theme smartparens)
+  '(spacemacs-theme smartparens rust-mode cargo flycheck markdown-mode lsp-mode lsp-rust exec-path-from-shell flycheck-rust racer lsp-ui projectile magit)
   "A list of packages to ensure are installed at launch.")
 
 (defun my-packages-installed-p ()
@@ -66,8 +65,85 @@
     (when (not (package-installed-p p))
       (package-install p))))
 
+(exec-path-from-shell-initialize)
+
+
+
+(global-flycheck-mode)
+
 (require 'smartparens-config)
+(require 'cargo)
+(require 'racer)
+(require 'projectile)
 
 (add-hook 'prog-mode-hook #'smartparens-mode)
 
 (load-theme 'spacemacs-dark t)
+
+(with-eval-after-load 'rust-mode
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+  (add-hook 'rust-mode-hook #'cargo-minor-mode)
+  (add-hook 'rust-mode-hook #'flycheck-mode)
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (setq company-tooltip-align-annotations t))
+
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+
+;; Stolen from (http://endlessparentheses.com/ansi-colors-in-the-compilation-buffer-output.html)
+(require 'ansi-color)
+(defun endless/colorize-compilation ()
+  "Colorize from `compilation-filter-start' to `point'."
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region
+     compilation-filter-start (point))))
+
+(add-hook 'compilation-filter-hook
+          #'endless/colorize-compilation)
+
+;; Stolen from (https://oleksandrmanzyuk.wordpress.com/2011/11/05/better-emacs-shell-part-i/)
+(defun regexp-alternatives (regexps)
+  "Return the alternation of a list of regexps."
+  (mapconcat (lambda (regexp)
+               (concat "\\(?:" regexp "\\)"))
+             regexps "\\|"))
+
+(defvar non-sgr-control-sequence-regexp nil
+  "Regexp that matches non-SGR control sequences.")
+
+(setq non-sgr-control-sequence-regexp
+      (regexp-alternatives
+       '(;; icon name escape sequences
+         "\033\\][0-2];.*?\007"
+         ;; non-SGR CSI escape sequences
+         "\033\\[\\??[0-9;]*[^0-9;m]"
+         ;; noop
+         "\012\033\\[2K\033\\[1F"
+         )))
+
+(defun filter-non-sgr-control-sequences-in-region (begin end)
+  (save-excursion
+    (goto-char begin)
+    (while (re-search-forward
+            non-sgr-control-sequence-regexp end t)
+      (replace-match ""))))
+
+(defun filter-non-sgr-control-sequences-in-output (ignored)
+  (let ((start-marker
+         (or comint-last-output-start
+             (point-min-marker)))
+        (end-marker
+         (process-mark
+          (get-buffer-process (current-buffer)))))
+    (filter-non-sgr-control-sequences-in-region
+     start-marker
+     end-marker)))
+
+(add-hook 'comint-output-filter-functions
+          'filter-non-sgr-control-sequences-in-output)
+
+(provide 'init)
+;;; init.el ends here
