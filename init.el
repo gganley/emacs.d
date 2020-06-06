29ca2e8311c2bfabcc1e6b554ee6cc0b46faed9f
Gregory Ganley <gcganley2854@gmail.com>
2020-06-06 08:42:55 -0400

    something

--- init.el
+++ init.el
@@ -32,7 +32,20 @@
   (setq-default
    use-package-always-defer t
    use-package-always-ensure t)
-
+  
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+  
   ;; Use latest Org
   (use-package org :ensure org-plus-contrib)
 
@@ -46,9 +59,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" default))
  '(doom-themes-enable-bold nil)
+ '(org-clock-mode-line-total 'today)
+ '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(aggressive-indent counsel-projectile org-plus-contrib which-key use-package-ensure-system-package smex smartparens projectile powerline neotree magithub git-gutter-fringe doom-themes doom-modeline diminish darcula-theme counsel avy)))
+   '(aggressive-indent counsel-projectile org-plus-contrib which-key use-package-ensure-system-package smex smartparens projectile powerline neotree magithub git-gutter-fringe doom-themes doom-modeline diminish darcula-theme counsel avy))
+ '(ring-bell-function 'ignore))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
